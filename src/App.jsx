90c520067b2f1493da1364fc4b99fee58771571e
jacklien <jacklienkamal442@gmail.com>
2024-06-15 02:40:24 +0300

    components

---
+++ src/App.jsx
@@ -0,0 +1,21 @@
+
+import './App.css'
+import Header from './components/Header/Header'
+import TopHeader from './components/Header/TopHeader'
+import Home from './pages/Home'
+
+function App() {
+
+  return (
+    <>
+    <div className="font-gt-america ">
+      <TopHeader/>
+    <Header/>
+    <Home/>
+    </div>
+  
+    </>
+  )
+}
+
+export default App
---
+++ tailwind.config.js
@@ -0,0 +1,15 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [
+    "./index.html",
+    "./src/**/*.{js,ts,jsx,tsx}",
+  ],  theme: {
+    fontFamily: {
+      'gt-america': ['Sora', 'sans-serif'],
+      'handlee': ["Handlee", 'system-ui']
+
+    },
+  },
+  plugins: [],
+}
+
